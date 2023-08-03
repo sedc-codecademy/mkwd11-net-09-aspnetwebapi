db50216631d182e3d22f822f10a714de7aa641dd
vjakovlev <vjakovlev89@gmail.com>
2023-08-03 20:50:10 +0200

    class 01 finished

---
+++ G2/Class01/OurFirstWebApi/OurFirstWebApi/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OurFirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] LoginModel loginModel) 
+        {
+            return Ok(loginModel.Username);
+        }
+    }
+
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ G2/Class01/OurFirstWebApi/OurFirstWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OurFirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("GetStrings")]
+        public List<string> GetStrings() 
+        {
+            return new List<string> { "value1", "value2" };
+        }
+
+        [HttpPost("GetStringsPost")]
+        public List<string> GetStringsPost()
+        {
+            return new List<string> { "value1", "value2" };
+        }
+    }
+}
