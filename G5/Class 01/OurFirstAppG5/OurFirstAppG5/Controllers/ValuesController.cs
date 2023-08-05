fef4948050659401169cb1d0124f375d7e9758e0
tijanastojanovska <stojanovska_tijana@outlook.com>
2023-08-05 12:38:30 +0200

    Class 01

---
+++ G5/Class 01/OurFirstAppG5/OurFirstAppG5/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OurFirstAppG5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("strings")]
+        public IEnumerable<string> GetStrings()
+        {
+            return new List<string> { "value1", "value2" };
+        }
+
+        [HttpGet("string")]
+        public string GetString()
+        {
+            return "value3";
+        }
+
+    }
+}
