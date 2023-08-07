ab0a12faa9aabb7daed02b5352059a2ed70e1ccc
Bojan Damchevski <bod@proofficepark.dk>
2023-08-07 20:18:34 +0200

    Code class02 added

---
+++ G1/Class02/SEDC.NotesApp/SEDC.NotesApp/Controllers/NotesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.NotesApp.Entities;
+
+namespace SEDC.NotesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        [HttpGet] //http://localhost:[port]/api/Notes
+        public ActionResult<List<string>> Get()
+        {
+            return Ok(StaticDb.SimpleNotes);
+        }
+
+        [HttpGet("{index?}")] //http://localhost:[port]/api/Notes/{index}
+        public ActionResult<string> GetByIndex(int? index)
+        {
+            try
+            {
+                if (index < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "The index is a negative number.");
+                }
+
+                if (index >= StaticDb.SimpleNotes.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"The note with index {index} was not found.");
+                }
+
+                return Ok(StaticDb.SimpleNotes[(int)index]);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured. Contact the support team.");
+            }
+        }
+
+        [HttpGet("{noteId}/user/{userId}")] //http://localhost:[port]/api/Notes/{noteId}/user/{userId}
+        public ActionResult<string> GetNoteByIdAndUserId(int noteId, int userId)
+        {
+            if (noteId < 0 || userId < 0)
+            {
+                return BadRequest("The ids can not be negative.");
+            }
+
+            return Ok($"Returning note with id {noteId} and user with id {userId}.");
+        }
+
+        [HttpPost] //http://localhost:[port]/api/Notes POST METHOD !!!
+        public IActionResult Post(NoteModel noteModel)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(noteModel.Text))
+                {
+                    return BadRequest("The body of the request can not be empty.");
+                }
+
+                StaticDb.SimpleNotes.Add(noteModel.Text);
+                return StatusCode(StatusCodes.Status201Created, "The new note was created");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured. Contact the support team.");
+            }
+        }
+    }
+}
---
+++ G1/Class02/SEDC.NotesApp/SEDC.NotesApp/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SEDC.NotesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet] //http://localhost:[port]/api/Values
+        public List<string> Get()
+        {
+            return new List<string>() { "Bojan", "Toshe", "Kostadin" };
+        }
+
+        [HttpGet("info")] //http://localhost:[port]/api/Values/info
+        public string GetInfo()
+        {
+            return "This is some info for our response.";
+        }
+
+        //This can not be accessed, because the URL is the same for some other endpoint. So there will be an API Error
+        //[HttpGet]
+        //public string GetString()
+        //{
+        //    return "Some text.";
+        //}
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Ok";
+        }
+
+        [HttpGet("details/{id}")]
+        public int GetById(int id)
+        {
+            return id;
+        }
+    }
+}
