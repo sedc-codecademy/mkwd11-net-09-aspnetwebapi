fa0c5d5242dd919faa3dc59980f28b095e39d74e
Bojan Damchevski <bod@proofficepark.dk>
2023-08-09 20:14:52 +0200

    Code class03 added

---
+++ G1/Class03/SEDC.NotesAndTagsApp/SEDC.NotesAndTagsApp/StaticDb.cs
@@ -0,0 +1,66 @@
+﻿using SEDC.NotesAndTagsApp.Models;
+using SEDC.NotesAndTagsApp.Models.Enums;
+
+namespace SEDC.NotesAndTagsApp
+{
+    public static class StaticDb
+    {
+        public static List<Note> Notes = new List<Note>()
+        {
+            new Note()
+            {
+                Text = "Learn MVC",
+                Priority = Priority.High,
+                Tags = new List<Tag>()
+                {
+                    new Tag()
+                    {
+                        Color = "red",
+                        Name = "Coding"
+                    },
+                    new Tag()
+                    {
+                        Color = "blue",
+                        Name = "SEDC"
+                    }
+                }
+            },
+            new Note()
+            {
+                Text = "Learn API",
+                Priority = Priority.Medium,
+                Tags = new List<Tag>()
+                {
+                    new Tag()
+                    {
+                        Color = "red",
+                        Name = "Coding"
+                    },
+                    new Tag()
+                    {
+                        Color = "blue",
+                        Name = "SEDC"
+                    }
+                }
+            },
+            new Note()
+            {
+                Text = "Learn JS",
+                Priority = Priority.Low,
+                Tags = new List<Tag>()
+                {
+                    new Tag()
+                    {
+                        Color = "yellow",
+                        Name = "Design"
+                    },
+                    new Tag()
+                    {
+                        Color = "blue",
+                        Name = "SEDC"
+                    }
+                }
+            }
+        };
+    }
+}
