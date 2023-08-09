f17b20140304986b01ec96847dca89d750e87f75
tijanastojanovska <stojanovska_tijana@outlook.com>
2023-08-09 20:57:22 +0200

    Class 03

---
+++ G5/Class 03/NotesAndTagsAppG5/NotesAndTagsAppG5/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿using NotesAndTagsAppG5.Models.Enums;
+
+namespace NotesAndTagsAppG5.Models
+{
+    public class Note
+    {
+        public string Text { get; set; }
+
+        public List<Tag> Tags { get; set; } 
+
+        public Priority Priority { get; set; }
+    }
+}
