c1772bc2f69045d9b718bbab7d613f6acf15eb70
Jovan Gjorgjiev <joki.joksi@hotmail.com>
2023-08-11 19:23:11 +0200

    Add books api

---
+++ G3/class4/SEDC.Books/Books.Api/Controllers/AuthorController.cs
@@ -0,0 +1,46 @@
+﻿using Books.Api.Data;
+using Books.Api.Dtos;
+using Books.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Books.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly BooksDbContext dbContext;
+
+        public AuthorController(BooksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var result = dbContext.Authors.Select(x => new AuthorDto
+            {
+                Id = x.Id,
+                Name = x.Name,
+                DateOfBirth = x.BirthYear
+            }).ToList();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthor(CreateAuthorDto dto)
+        {
+            var author = new Author
+            {
+                BirthYear = dto.DateOfBirth,
+                Name = dto.Name
+            };
+            dbContext.Authors.Add(author);
+            dbContext.SaveChanges();
+            return Ok(author);
+        }
+    }
+}
---
+++ G3/class4/SEDC.Books/Books.Api/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace Books.Api.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+
+        public decimal Price { get; set; }
+
+        public ICollection<Author> Authors { get; set; } = new List<Author>();
+    }
+}
