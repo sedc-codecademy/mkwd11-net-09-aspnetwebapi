d27e7878af92db1a9ff20bfbc11b50d9a9a37d92
Bojan Damchevski <bod@proofficepark.dk>
2023-08-11 18:17:35 +0200

    More initial code

---
+++ G1/Class04/SEDC.NotesAndTagsApp2/SEDC.NotesAndTagsApp2/Controllers/NotesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SEDC.NotesAndTagsApp2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        //[HttpGet]
+        //public ActionResult<List<NoteDTO>> Get()
+
+        //[HttpGet("{id}")]
+        //public ActionResult<NoteDTO> GetNoteById()
+
+        //[HttpGet("findById")] //Query string !!!
+        //public ActionResult<List<NoteDTO>> FindById()
+
+        //[HttpPut]
+        //public IActionResult Put([FromBody] UpdateNoteDto updateNoteDto)
+
+        //[HttpDelete("{id}")]
+        //public IActionResult DeleteById(int id)
+
+        //[HttpGet("user/{userId}")]
+        //public ActionResult<List<NoteDTO>> GetNotesByUser(int userId)
+
+        //[HttpPost("addNote")]
+        //public IActionResult AddNote([FromBody] AddNoteDto addNoteDto)
+    }
+}
