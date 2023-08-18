4fd190c50b4ccb8f5c8c874393b805cc0768a9b5
Bojan Damchevski <bod@proofficepark.dk>
2023-08-18 20:30:30 +0200

    Code class07 added

---
+++ G1/Class07/SEDC.NotesAppFinal/SEDC.NotesAppFinal.Domain/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace SEDC.NotesAppFinal.Domain.Models
+{
+    public class User : BaseEntity
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+
+        public List<Note> Notes { get; set; } = new List<Note>();
+    }
+}
---
+++ G1/Class07/SEDC.NotesAppFinal/SEDC.NotesAppFinal.Services/Implementations/NotesService.cs
@@ -0,0 +1,30 @@
+﻿using SEDC.NotesAppFinal.DataAccess.Interfaces;
+using SEDC.NotesAppFinal.Domain.Models;
+using SEDC.NotesAppFinal.DTOs.NoteDTOs;
+using SEDC.NotesAppFinal.Mappers;
+using SEDC.NotesAppFinal.Services.Interfaces;
+
+namespace SEDC.NotesAppFinal.Services.Implementations
+{
+    public class NotesService : INotesService
+    {
+        private readonly IRepository<Note> _noteRepository;
+
+        public NotesService(IRepository<Note> noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
+        public async Task<NoteDto> GetNoteAsync(int id)
+        {
+            Note noteDb = await _noteRepository.GetByIdAsync(id);
+
+            if (noteDb == null)
+            {
+                throw new Exception("Note is null");
+            }
+
+            return noteDb.MapToNoteDto();
+        }
+    }
+}
