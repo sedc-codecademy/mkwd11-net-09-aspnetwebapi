d641070a190c1fda3089786fff98233c2306e087
tijanastojanovska <stojanovska_tijana@outlook.com>
2023-08-21 20:53:24 +0200

    Class 07

---
+++ G5/Class 07/NotesAndTagsApp/NotesAndTagsApp/Controllers/NotesController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NotesAndTagsApp.DTOs;
+using NotesAndTagsApp.Services.Interfaces;
+using NotesAndTagsApp.Shared.CustomExceptions;
+
+namespace NotesAndTagsApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+
+        public NotesController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<NoteDto>> GetAll()
+        {
+            try
+            {
+                return Ok(_noteService.GetAllNotes());
+
+            }
+            catch(Exception ex) { 
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<NoteDto> GetById(int id)
+        {
+            try
+            {
+                var noteDto = _noteService.GetById(id); //potential NoteNotFoundException
+                return Ok(noteDto); //status 200
+            }
+            catch (NoteNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost("addNote")]
+        public IActionResult AddNote([FromBody] AddNoteDto addnoteDto)
+        {
+            try
+            {
+                _noteService.AddNote(addnoteDto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
