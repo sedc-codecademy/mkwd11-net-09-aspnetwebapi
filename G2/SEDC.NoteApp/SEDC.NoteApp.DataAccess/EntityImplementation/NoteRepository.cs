ae4c22673a00e01067e952981c632f246549c8c6
ilijamitev <ilija_mitev_mk@yahoo.com>
2023-08-26 12:46:19 +0200

    Class 8 - ORMs

--- G2/SEDC.NoteApp/SEDC.NoteApp.DataAccess/EntityImplementation/NoteRepository.cs
+++ G2/SEDC.NoteApp/SEDC.NoteApp.DataAccess/EntityImplementation/NoteRepository.cs
@@ -25,9 +25,20 @@ namespace SEDC.NoteApp.DataAccess.EntityImplementation
 
         public List<Note> GetAll()
         {
-            return _noteAppDbContext.Notes
-                        .Include(x => x.User)
-                        .ToList();
+            //return _noteAppDbContext.Notes
+            //            .Include(x => x.User)
+            //            .ToList();
+
+            // EntityFramework behind the scenes
+            // we create the query in C#
+            var query = _noteAppDbContext.Notes
+                            .Include(x => x.User);
+            // useful tool to check how our LINQ query is translated to SQL query
+            var translatedQuery = query.ToQueryString(); 
+            // when calling ToList() our query is translated into SQL query => executed in the database => retrives data
+            var materializedQuery = query.ToList();
+            return materializedQuery;
+            // EntityFramework Executable methods: ToList(), FirstOrDefault(), Any(), SingleOrDefault(), Count()...
         }
 
         public Note GetById(int id)
