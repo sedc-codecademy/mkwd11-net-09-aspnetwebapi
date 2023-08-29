385b50c4b3b94bde8a84ac6e6a2a89d268395bb0
tstojanovska <stojanovskatanja03@gmail.com>
2023-08-29 20:53:37 +0200

    Class 10

---
+++ G6/Class10 - Authentication part1/Code/SEDC.NotesApp/SEDC.NotesApp/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.NotesApp.Dtos.Users;
+using SEDC.NotesApp.Services.Interfaces;
+using SEDC.NotesApp.Shared.Shared;
+
+namespace SEDC.NotesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterUserDto registerUserDto)
+        {
+            //call some service
+            try
+            {
+                _userService.RegisterUser(registerUserDto);
+                return Ok();
+            }
+            catch (DataException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                //log
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred, contact the admin!");
+            }
+        }
+    }
+}
