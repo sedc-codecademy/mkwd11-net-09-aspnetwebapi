1e16d5e4b813f7074af2b8bac4903fe3721117a2
Dragan Manaskov <dmanaskov@hotmail.com>
2023-08-31 21:07:56 +0200

    Class 11

---
+++ G7/Class11/SEDC.MoviesApp/SEDC.MoviesApp.DataAccess/Migrations/20230826092047_FirstMigration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SEDC.MoviesApp.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Genre = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Genre", "Title", "Year" },
+                values: new object[,]
+                {
+                    { 1, "Documentary", 4, "Oppenheimer", 2023 },
+                    { 2, "Girl", 1, "Barbie", 2023 },
+                    { 3, "Action movie", 2, "Top Gun:Maverick", 2021 },
+                    { 4, "Action movie", 1, "Dumb and dumber", 1994 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
