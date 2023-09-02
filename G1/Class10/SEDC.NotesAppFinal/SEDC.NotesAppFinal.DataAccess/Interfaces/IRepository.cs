89b3dd139e8ef3568ffa12b5f4d951555f90c866
Bojan Damchevski <bod@proofficepark.dk>
2023-09-02 12:15:14 +0200

    Code class10 added

---
+++ G1/Class10/SEDC.NotesAppFinal/SEDC.NotesAppFinal.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace SEDC.NotesAppFinal.DataAccess.Interfaces
+{
+    using SEDC.NotesAppFinal.Domain.Models;
+
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+
+        Task<List<T>> GetAllAsync();
+
+        Task CreateAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(int id);
+    }
+}
