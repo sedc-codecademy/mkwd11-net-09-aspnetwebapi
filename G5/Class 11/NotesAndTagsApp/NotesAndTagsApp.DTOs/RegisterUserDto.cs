de86aba52946cba6f05a046e47dfde1527c9b2f7
Danilo Borozan <daniloborozan07@gmail.com>
2023-09-04 21:39:48 +0200

    Add code for class11

---
+++ G5/Class 11/NotesAndTagsApp/NotesAndTagsApp.DTOs/RegisterUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesAndTagsApp.DTOs
+{
+    public class RegisterUserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ G5/Class 11/NotesAndTagsApp/NotesAndTagsApp/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NotesAndTagsApp.DTOs;
+using NotesAndTagsApp.Services.Interfaces;
+
+namespace NotesAndTagsApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService) {
+            _userService = userService;
+        }
+
+        [AllowAnonymous] //no token needed (we can not be logged in before registration)
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterUserDto registerUserDto)
+        {
+            try
+            {
+                _userService.Register(registerUserDto);
+                return StatusCode(StatusCodes.Status201Created, "User was created");
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [AllowAnonymous] //no token needed (we can not be logged in before login)
+        [HttpPost("login")]
+        public IActionResult LoginUser([FromBody] LoginDto loginDto)
+        {
+            try
+            {
+                string token = _userService.LoginUser(loginDto);
+                return Ok(token);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred!");
+            }
+        }
+    }
+}
