682658699e7199d98c9346aed4cff06026c438d1
Danilo Borozan <daniloborozan07@gmail.com>
2023-09-06 17:43:10 +0200

    Workshop part2 code

---
+++ G5/Class 12/SEDC.MoviesApp/SEDC.MoviesApp.DataAccess/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using SEDC.MoviesApp.Domain;
+using SEDC.MoviesApp.Domain.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.MoviesApp.DataAccess
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        User GetUserByUsername(string username);
+        User LoginUser(string username, string hashedPassword);
+    }
+}
---
+++ G5/Class 12/SEDC.MoviesApp/SEDC.MoviesApp.Services/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using SEDC.MoviesApp.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.MoviesApp.Services.Interfaces
+{
+    public interface IUserService
+    {
+        UserDto Authenticate(LoginModelDto model);
+        void Register(RegisterModelDto model);
+        string LoginUser(LoginModelDto loginDto);
+    }
+}
