c3f9f61e4e80c821b4a511badbc3fd9a0c3fac8c
vjakovlev <vjakovlev89@gmail.com>
2023-09-07 20:16:23 +0200

    class 12 updated

---
+++ G2/Class12/Unit_Testing/UnitTesting_Services/RandomService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting_Services
+{
+    public class RandomService
+    {
+        public int? Sum(int num1, int num2)
+        {
+            var res = num1 + num2;
+            if (num1 > 0 && num2 > 0 && res < 0) 
+            {
+                return null;
+            };
+            return res;
+        }
+        public bool IsFirstNumberLarger(int num1, int num2)
+        {
+            return num1 > num2;
+        }
+
+        public string GetDigitName(int num)
+        {
+            List<string> digitNames = new List<string>()
+            {
+                "Zero", 
+                "One", 
+                "Two", 
+                "Three", 
+                "Four", 
+                "Five", 
+                "Six", 
+                "Seven", 
+                "Eight", 
+                "Nine"
+            };
+            return digitNames[num];
+        }
+    }
+}
---
+++ G2/Class12/Unit_Testing/UnitTesting_Tests/RandomServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTesting_Services;
+
+namespace UnitTesting_Tests
+{
+    [TestClass]
+    public class RandomServiceTests
+    {
+        private readonly RandomService _randomService;
+        public RandomServiceTests()
+        {
+            _randomService = new RandomService();
+        }
+
+        // naming unit tests example
+        // MethodName_StateUnderTest_ExpectedBehavior
+        [TestMethod]
+        public void Sum_BothNumbersAreValid_PositiveResultNumber() 
+        {
+            //Arrange
+            var number1 = 5;
+            var number2 = 10;
+            var expectedResult = 15;
+
+            //Act
+            var result = _randomService.Sum(number1, number2);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void IsFirstNumberLarger_FirstNumberIsLarger_True() 
+        {
+            //Arrange
+            int number1 = 10;
+            int number2 = 5;
+
+            //Act
+            var result = _randomService.IsFirstNumberLarger(number1, number2);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
